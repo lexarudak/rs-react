27e35f48f0bf3c6aa8590f9fb6f1049212615062
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-14 17:40:13 +0600

    feat: add app style

--- components/src/components/layout/layout.tsx
+++ components/src/components/layout/layout.tsx
@@ -13,18 +13,21 @@ class Layout extends React.Component<{ name: PageNames }> {
     return (
       <>
         <header className="header">
-          <h2 className="header__name">{this.props.name}</h2>
-          <Link className="header__link" to={RouteList.homePage}>
-            Home
-          </Link>
-          <Link className="header__link" to={RouteList.aboutPage}>
-            About Us
-          </Link>
+          <h2 className="header__name">React Components.</h2>
+          <span className="header__page-name">/ {this.props.name}</span>
+          <nav className="header__nav">
+            <Link className="header__link" to={RouteList.homePage}>
+              Home
+            </Link>
+            <Link className="header__link" to={RouteList.aboutPage}>
+              About Us
+            </Link>
+          </nav>
         </header>
-
-        <Outlet />
-
-        <footer>2023 (c)</footer>
+        <main className="main">
+          <Outlet />
+        </main>
+        <footer className="footer">2023 (c)</footer>
       </>
     );
   }
