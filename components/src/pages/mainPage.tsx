23a319c0c0b0aa6e90e058d784e1f49985c5a2b9
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-15 06:58:37 +0600

    feat: add search component without functionality

--- components/src/pages/mainPage.tsx
+++ components/src/pages/mainPage.tsx
@@ -1,3 +1,5 @@
+import Search from '../components/input/search';
+import React from 'react';
 import PageNames from '../base/enums/pageNames';
 import PageProps from '../base/types';
 import Page from './page';
@@ -5,6 +7,14 @@ import Page from './page';
 class MainPage extends Page {
   constructor(props: PageProps) {
     super(props, PageNames.mainPage);
+  }
+
+  public render() {
+    return (
+      <div className="main__container">
+        <Search />
+      </div>
+    );
   }
 }
 
