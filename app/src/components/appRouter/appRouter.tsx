d6feedbfb90335821a94fe32389fc1b97bed3971
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-28 11:46:21 +0600

    refactor: retain the functionality to the app component

--- app/src/components/appRouter/appRouter.tsx
+++ app/src/components/appRouter/appRouter.tsx
@@ -4,40 +4,24 @@ import Layout from '../layout/layout';
 import AboutPage from '../../pages/aboutPage/aboutPage';
 import ErrorPage from '../../pages/errorPage/errorPage';
 import MainPage from '../../pages/mainPage/mainPage';
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import FormsPage from '../../pages/formsPage/formsPage';
 
-class AppRouter extends React.Component {
-  state: Readonly<{ pageName: PageNames }>;
+function App() {
+  const [pageName, changePageName] = useState(PageNames.mainPage);
 
-  constructor(props: string) {
-    super(props);
-
-    this.state = {
-      pageName: PageNames.mainPage,
-    };
-
-    this.setPageName = this.setPageName.bind(this);
-  }
-
-  private setPageName(name: PageNames) {
-    this.setState({ pageName: name });
-  }
-
-  public render() {
-    return (
-      <Routes>
-        <Route path={RouteList.homePage} element={<Layout name={this.state.pageName} />}>
-          <Route index element={<MainPage changeName={this.setPageName} />} />
-          <Route path={RouteList.aboutPage} element={<AboutPage changeName={this.setPageName} />} />
-          <Route path={RouteList.errorPage} element={<ErrorPage changeName={this.setPageName} />} />
-          <Route path={RouteList.formsPage} element={<FormsPage changeName={this.setPageName} />} />
-          <Route path="*" element={<Navigate to={RouteList.errorPage} />} />
-        </Route>
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route path={RouteList.homePage} element={<Layout name={pageName} />}>
+        <Route index element={<MainPage changeName={changePageName} />} />
+        <Route path={RouteList.aboutPage} element={<AboutPage changeName={changePageName} />} />
+        <Route path={RouteList.errorPage} element={<ErrorPage changeName={changePageName} />} />
+        <Route path={RouteList.formsPage} element={<FormsPage changeName={changePageName} />} />
+        <Route path="*" element={<Navigate to={RouteList.errorPage} />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default AppRouter;
+export default App;
