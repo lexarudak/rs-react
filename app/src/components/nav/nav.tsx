7f0ff67a185a1bc2986352b4d6c02a8bb3f44268
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-28 12:11:04 +0600

    refactor: retain the functionality to the nav component

--- app/src/components/nav/nav.tsx
+++ app/src/components/nav/nav.tsx
@@ -3,26 +3,20 @@ import RouteList from '../../base/enums/routeList';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Nav extends React.Component {
-  constructor(props: string) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <nav className="nav">
-        <Link className="nav__link" to={RouteList.homePage}>
-          {PageNames.mainPage}
-        </Link>
-        <Link className="nav__link" to={RouteList.formsPage}>
-          {PageNames.formsPage}
-        </Link>
-        <Link className="nav__link" to={RouteList.aboutPage}>
-          {PageNames.aboutPage}
-        </Link>
-      </nav>
-    );
-  }
+function Nav() {
+  return (
+    <nav className="nav">
+      <Link className="nav__link" to={RouteList.homePage}>
+        {PageNames.mainPage}
+      </Link>
+      <Link className="nav__link" to={RouteList.formsPage}>
+        {PageNames.formsPage}
+      </Link>
+      <Link className="nav__link" to={RouteList.aboutPage}>
+        {PageNames.aboutPage}
+      </Link>
+    </nav>
+  );
 }
 
 export default Nav;
