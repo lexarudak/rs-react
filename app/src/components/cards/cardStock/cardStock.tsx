ba5ee73eee9b8c5a44e71813ee57c987da51710e
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-29 17:37:55 +0600

    refactor: retain the functionality to the card stock component

--- app/src/components/cards/cardStock/cardStock.tsx
+++ app/src/components/cards/cardStock/cardStock.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-class CardStock extends React.Component<{ stock: number }> {
-  constructor(props: { stock: number }) {
-    super(props);
-  }
-
-  private setStock() {
-    const stock = Math.floor(this.props.stock);
-
-    if (stock > 1000) return <div className="card__stock">more than 1000 in stock</div>;
-    if (stock > 0) return <div className="card__stock">in stock: {stock}</div>;
+function CardStock(props: { stock: number }) {
+  function setStock(stock: number) {
+    const stockFloor = Math.floor(stock);
+    if (stockFloor > 1000) return <div className="card__stock">more than 1000 in stock</div>;
+    if (stockFloor > 0) return <div className="card__stock">in stock: {stockFloor}</div>;
     return <div className="card__stock card__stock_null">not in stock</div>;
   }
 
-  public render() {
-    return <>{this.setStock()}</>;
-  }
+  return <>{setStock(props.stock)}</>;
 }
 
 export default CardStock;
