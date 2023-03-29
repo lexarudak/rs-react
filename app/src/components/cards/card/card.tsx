67c16bb85335c424597315efc6dc205339d63513
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-29 17:23:07 +0600

    refactor: retain the functionality to the card component

--- app/src/components/cards/card/card.tsx
+++ app/src/components/cards/card/card.tsx
@@ -5,26 +5,20 @@ import CardImg from '../cardImg/cardImg';
 import CardPrice from '../cardPrice/cardPrice';
 import CardStock from '../cardStock/cardStock';
 
-class Card extends React.Component<CardInfo> {
-  constructor(props: CardInfo) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <div className="card" data-testid="card">
-        <CardImg {...this.props} />
-        <div className="card__type">{this.props.type}</div>
-        <div className="card__title">{this.props.title}</div>
-        <div className="card__text">{this.props.description}</div>
-        <div className="card__price-and-button">
-          <CardPrice sale={this.props.sale} price={this.props.price} />
-          <CardButton {...this.props} />
-        </div>
-        <CardStock stock={this.props.stock} />
+function Card(props: CardInfo) {
+  return (
+    <div className="card" data-testid="card">
+      <CardImg {...props} />
+      <div className="card__type">{props.type}</div>
+      <div className="card__title">{props.title}</div>
+      <div className="card__text">{props.description}</div>
+      <div className="card__price-and-button">
+        <CardPrice sale={props.sale} price={props.price} />
+        <CardButton {...props} />
       </div>
-    );
-  }
+      <CardStock stock={props.stock} />
+    </div>
+  );
 }
 
 export default Card;
