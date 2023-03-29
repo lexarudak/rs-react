36366d9e0c2a58708efdd75ea3a1be206062dd9d
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-29 17:40:49 +0600

    refactor: retain the functionality to the form button component

--- app/src/components/buttons/formButton/formButton.tsx
+++ app/src/components/buttons/formButton/formButton.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 
-class FormButton extends React.Component<{
-  name: string;
-  callback: (event: React.MouseEvent) => void;
-}> {
-  constructor(props: { name: string; callback: (event: React.MouseEvent) => void }) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <button className="card-button" onClick={(event) => this.props.callback(event)}>
-        {this.props.name}
-      </button>
-    );
-  }
+function FormButton(props: { name: string; callback: (event: React.MouseEvent) => void }) {
+  return (
+    <button className="card-button" onClick={(event) => props.callback(event)}>
+      {props.name}
+    </button>
+  );
 }
 
 export default FormButton;
