61c0f06adcd8998940dc785d454f0353b6aeb2ca
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-31 16:36:10 +0600

    refactor: retain the functionality to the popup with text component

--- app/src/components/popup/popupWithText.tsx
+++ app/src/components/popup/popupWithText.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 
-class PopupWithText extends React.Component<{ text: string; isPopupShow: boolean }> {
-  constructor(props: { text: string; isPopupShow: boolean }) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div
-        data-testid="popup-with-text"
-        className={
-          this.props.isPopupShow ? 'popup-with-text popup-with-text_active' : 'popup-with-text'
-        }
-      >
-        <h2 className="popup-with-text__text">{this.props.text}</h2>
-      </div>
-    );
-  }
+function PopupWithText(props: { text: string; isPopupShow: boolean }) {
+  return (
+    <div
+      data-testid="popup-with-text"
+      className={props.isPopupShow ? 'popup-with-text popup-with-text_active' : 'popup-with-text'}
+    >
+      <h2 className="popup-with-text__text">{props.text}</h2>
+    </div>
+  );
 }
 
 export default PopupWithText;
