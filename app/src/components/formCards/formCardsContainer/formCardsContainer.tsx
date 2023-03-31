e3457813babdab3a1f6b819128513dc7b6163f92
Aliaksei Rudak <lexarudak@gmail.com>
2023-03-31 16:33:57 +0600

    refactor: retain the functionality to the form cards container component

--- app/src/components/formCards/formCardsContainer/formCardsContainer.tsx
+++ app/src/components/formCards/formCardsContainer/formCardsContainer.tsx
@@ -2,23 +2,18 @@ import { FormPageCard } from 'base/types';
 import React from 'react';
 import FormCard from '../formCard/formCard';
 
-class FormCardsContainer extends React.Component<{ cards: FormPageCard[] }> {
-  constructor(props: { cards: FormPageCard[] }) {
-    super(props);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="form-cards-container">
-        <h2 className="form-cards-container__title title">Your cards</h2>
-        <div className="form-cards-container__cards">
-          {this.props.cards.map((card) => (
-            <FormCard {...card} key={card.key} />
-          ))}
-        </div>
+function FormCardsContainer(props: { cards: FormPageCard[] }) {
+  console.log(props.cards);
+  return (
+    <div className="form-cards-container">
+      <h2 className="form-cards-container__title title">Your cards</h2>
+      <div className="form-cards-container__cards">
+        {props.cards.map((card) => (
+          <FormCard {...card} key={card.key} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default FormCardsContainer;
